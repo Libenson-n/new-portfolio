8a6efa9aa9341ae593b524627e780d3854e55589
Libenson Nicolas <libenson.n@gmail.com>
2024-10-21 13:31:25 -0400

    hero update

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -1,21 +1,48 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin, faDiscord } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import Link from "next/link";
+import Image from "next/image";
 
 const Hero = () => {
   return (
-    <section className="h-screen max-w-[1000px] mx-auto px-32 flex justify-center  flex-col text-5xl">
-      <p>
-        Hi, I'm Libenson <br />
-        <span className="text-indigo-700 font-semibold">
-          Full-Stack Web Developer
-        </span>
+    <section className="h-screen max-w-[1000px] mx-auto px-32 flex justify-center  flex-col text-5xl gap-6">
+      <div className="flex items-center justify-between">
+        <p className="font-bold">
+          Hi, I'm Libenson <br />
+          <span className="text-indigo-700 font-semibold">A web developer</span>
+        </p>
+        <Image
+          src="/PORTRAIT.jpg"
+          height={200}
+          width={200}
+          alt="portrait"
+          className="rounded-full shadow-md border"
+        />
+      </div>
+      <p className="text-lg">
+        I'm passionate about creating responsive, user-friendly websites. With
+        expertise in HTML, CSS, JavaScript, TypeScript and various frameworks
+        and libraries like NextJS and ReactJS, I strive to build innovative
+        solutions that enhance user experience. I'm always eager to learn new
+        technologies and collaborate on exciting projects.
       </p>
-      <p className="text-lg">I love learning and creating things</p>
       <div className="flex gap-5 text-zinc-600">
-        <FontAwesomeIcon icon={faDiscord} className="h-8" />
-        <FontAwesomeIcon icon={faLinkedin} className="h-8" />
-        <FontAwesomeIcon icon={faEnvelope} className="h-8" />
+        <Link
+          href="https://github.com/Libenson-n"
+          target="_blank"
+          className="flex items-center gap-2 border rounded-full p-2 shadow-md bg-indigo-700 text-white"
+        >
+          <FontAwesomeIcon icon={faGithub} className="h-8" />
+          <p className="text-xs">GitHub</p>
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/libenson-nicolas/"
+          target="_blank"
+          className="flex items-center gap-2 border rounded-full px-2 shadow-md bg-indigo-700 text-white"
+        >
+          <FontAwesomeIcon icon={faLinkedin} className="h-8" />
+          <p className="text-xs">LinkedIn</p>
+        </Link>
       </div>
     </section>
   );
--- src/app/_components/Projects.tsx
+++ src/app/_components/Projects.tsx
@@ -1,7 +1,65 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
+import Link from "next/link";
+
 const Projects = () => {
   return (
     <section className="h-screen max-w-[1000px] mx-auto px-10">
-      Projects
+      <div>
+        <Card>
+          <CardHeader>
+            <Link
+              href={"https://my-recipe-book-tawny.vercel.app/"}
+              target="_blank"
+            >
+              <CardTitle>My Cookbook</CardTitle>
+              <CardDescription>
+                Tech Stack: <br /> NextJs, TypeScript, Tailwind CSS, PostgreSQL,
+                Prisma ORM
+              </CardDescription>
+            </Link>
+          </CardHeader>
+          <CardContent>
+            <Image
+              src={"/my-cookbook-home.png"}
+              height={200}
+              width={200}
+              alt="project screenshot"
+              className="rounded-lg shadow-lg"
+            />
+          </CardContent>
+          <CardFooter className="flex gap-5">
+            <Link
+              href={"https://github.com/Libenson-n/my-recipe-book"}
+              target="_blank"
+              className="flex items-center gap-2 border rounded-full p-2 shadow-md bg-indigo-700 text-white"
+            >
+              <p>
+                <FontAwesomeIcon icon={faGithub} className="h-4" />
+              </p>
+              <p>Repo</p>
+            </Link>
+
+            <Link
+              href={"https://my-recipe-book-tawny.vercel.app/"}
+              target="_blank"
+              className="flex items-center gap-2 border rounded-full p-2 shadow-md bg-indigo-700 text-white"
+            >
+              Live site
+            </Link>
+          </CardFooter>
+        </Card>
+      </div>
     </section>
   );
 };
